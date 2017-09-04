1e0e87fb57f201301f4b4abd543a091f6ca07329
David Blood <davidbjourno@users.noreply.github.com>
2017-09-04 14:20:06 +0100

    Do some stuff

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-var pg = require('pg');
-var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
+var cool = require('cool-ascii-faces');
+var url = require('url');
+var redis = require('redis');
+
+const client = getRedisConnection();
+
+client.on('error', function() {
+  console.log('App crashed');
+  process.exit(1);
+});
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -20,25 +28,46 @@ app.get('/cool', function(request, response) {
 });
 
 app.get('/times', function(request, response) {
-    var result = ''
-    var times = process.env.TIMES || 5
-    for (i=0; i < times; i++)
-      result += i + ' ';
+  var result = ''
+  var times = process.env.TIMES || 5
+
+  for (i=0; i < times; i++)
+    result += i + ' ';
+
   response.send(result);
 });
 
-// app.get('/db', function (request, response) {
-//   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-//     client.query('SELECT * FROM test_table', function(err, result) {
-//       done();
-//       if (err)
-//        { console.error(err); response.send("Error " + err); }
-//       else
-//        { response.render('pages/db', {results: result.rows} ); }
-//     });
-//   });
-// });
+app.get('/db', function (request, response) {
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  client.set('welcome_msg', 'Hello from Redis!');
+
+  client.get('welcome_msg', function (err, reply) {
+    if (reply != null) {
+      response.send(reply);
+    } else {
+      response.send('Error');
+    }
+  });
 });
+
+
+client.on('connect', function(){
+  console.log('ready')
+  app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+  });
+});
+
+function getRedisConnection() {
+  const local = !process.env.REDISCLOUD_URL;
+  const options = {no_ready_check: true};
+
+  if (local) {
+    return redis.createClient();
+  }
+
+  var redisURL = url.parse(process.env.REDISCLOUD_URL);
+  var client = redis.createClient(process.env.REDISCLOUD_URL || undefined, {no_ready_check: true});
+  client.auth(redisURL.auth.split(':')[1]);
+  return client;
+}
